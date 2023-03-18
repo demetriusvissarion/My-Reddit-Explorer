34c0a957c75f5ec9ccc15c46b10a018576d80e3e
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-03-18 08:51:26 +0000

    split Card into smaller components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,14 @@
-import Up from "../img/Up.svg";
-import Down from "../img/Down.svg";
-import bigExplorer from "../img/bigExplorer.webp";
+import { Vote } from "./Vote";
+import { CardFunction } from "./CardFunction";
+import { CardContent } from "./CardContent";
 import "./Card.css";
 
 export const Card = () => {
   return (
     <div className="Card">
-      <img src={Up} className="UpIcon" />
-      <img src={Down} className="DownIcon" />
-      <p className="Vote">26.1k</p>
-      <div className="ContentContainer">
-        <p className="Channel">
-          r/interestingasfuck •Posted by u/JarethKingofGoblins
-        </p>
-        <p className="Title">
-          AMC Theaters to Change Movie Ticket Prices Based on Seat Location
-        </p>
-        <div className="ImageContainer">
-          <img src={bigExplorer} id="image" />
-        </div>
-      </div>
+      <Vote />
+      <CardContent />
+      <CardFunction />
     </div>
   );
 };
---
+++ src/components/CardContent.js
@@ -0,0 +1,17 @@
+import bigExplorer from "../img/bigExplorer.webp";
+
+export const CardContent = () => {
+  return (
+    <div className="CardContent">
+      <p className="Channel">
+        r/interestingasfuck •Posted by u/JarethKingofGoblins
+      </p>
+      <p className="Title">
+        AMC Theaters to Change Movie Ticket Prices Based on Seat Location
+      </p>
+      <div className="ImageContainer">
+        <img src={bigExplorer} id="image" />
+      </div>
+    </div>
+  );
+};
